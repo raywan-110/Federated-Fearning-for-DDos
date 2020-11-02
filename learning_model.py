da8e838a2b52bf5c9e00572b66e3100c010f9164
raywan-110 <545981160@qq.com>
2020-11-02 20:15:35 +0800

    add  dropout layers to avoid overfitting

--- learning_model.py
+++ learning_model.py
@@ -7,12 +7,16 @@ class FLModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.net = nn.Sequential(nn.Linear(79, 128),
+                                 nn.Dropout(),
                                  nn.ReLU(),
                                  nn.Linear(128, 256),
+                                 nn.Dropout(),
                                  nn.ReLU(),
                                  nn.Linear(256, 512),
+                                 nn.Dropout(),
                                  nn.ReLU(),
                                  nn.Linear(512, 512),
+                                 nn.Dropout(),
                                  nn.ReLU(),
                                  nn.Linear(512, 14))  # 5 layers
 
